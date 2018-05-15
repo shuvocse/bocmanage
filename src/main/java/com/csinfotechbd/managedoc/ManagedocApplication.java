c3c4ef5f264ce56e40dcf4426ad25b3dba9011b9
Emon Hossain <emondipu@gmail.com>
2018-05-15 12:14:58 +0600

    init

---
+++ src/main/java/com/csinfotechbd/managedoc/ManagedocApplication.java
@@ -0,0 +1,12 @@
+package com.csinfotechbd.managedoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagedocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagedocApplication.class, args);
+	}
+}
---
+++ src/test/java/com/csinfotechbd/managedoc/ManagedocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.csinfotechbd.managedoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagedocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
